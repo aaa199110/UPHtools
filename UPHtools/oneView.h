95cdb6ff1ee796768de56a50dd0ce4274dd302f4
chenlei <leichen@sysnew.com>
2016-07-19 11:16:44 +0800

    Release 1.0.0.

---
+++ UPHtools/oneView.h
@@ -0,0 +1,19 @@
+//
+//  oneView.h
+//  WHC_ ContainerView
+//
+//  Created by 吴海超 on 15/5/18.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+/*
+ *  qq:712641411
+ *  gitHub:https://github.com/netyouli
+ *  csdn:http://blog.csdn.net/windwhc/article/category/3117381
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface oneView : UIView
+
+@end
---
+++ UPHtools/oneView.m
@@ -0,0 +1,23 @@
+//
+//  oneView.m
+//  WHC_ ContainerView
+//
+//  Created by 吴海超 on 15/5/18.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+/*
+ *  qq:712641411
+ *  gitHub:https://github.com/netyouli
+ *  csdn:http://blog.csdn.net/windwhc/article/category/3117381
+ */
+
+#import "oneView.h"
+@interface oneView ()
+@end
+
+@implementation oneView
+
+
+
+@end
